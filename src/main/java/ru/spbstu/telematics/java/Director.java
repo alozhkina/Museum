c3bce2b7869f3178977a358d92c51b26c935b971
Anastasiya <Lozhkina_Anastasya@list.ru>
2024-12-18 15:12:16 +0300

    add Director's job

--- src/main/java/ru/spbstu/telematics/java/Director.java
+++ src/main/java/ru/spbstu/telematics/java/Director.java
@@ -1,4 +1,23 @@
 package ru.spbstu.telematics.java;
 
-public class Director {
+public class Director implements Runnable {
+    private final Control control;
+
+    public Director(Control control) {
+        this.control = control;
+    }
+
+    @Override
+    public void run() {
+        try {
+            for (int i = 0; i < 3; i++) { // Музей открывается и закрывается 3 раза
+                control.openMuseum();
+                Thread.sleep(5000); // Музей работает 5 секунд
+                control.closeMuseum();
+                Thread.sleep(2000); // Перерыв перед следующим открытием
+            }
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
 }
