86bf846e8c5e4a90c209e61430265db5371b6d65
Anastasiya <Lozhkina_Anastasya@list.ru>
2024-12-18 15:31:02 +0300

    add WestExit

--- src/main/java/ru/spbstu/telematics/java/West.java
+++ src/main/java/ru/spbstu/telematics/java/West.java
@@ -1,4 +1,24 @@
 package ru.spbstu.telematics.java;
 
-public class West {
+public class West implements Runnable {
+    private final Control control;
+
+    public West(Control control) {
+        this.control = control;
+    }
+
+    @Override
+    public void run() {
+        int visitorId = 1; // Уникальный идентификатор для выхода
+        while (true) {
+            try {
+                Thread.sleep(400);
+                control.exitMuseum(visitorId++); // Посетитель выходит из музея
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                break; // Завершаем поток при прерывании
+            }
+        }
+        System.out.println("West: Поток завершен.");
+    }
 }
