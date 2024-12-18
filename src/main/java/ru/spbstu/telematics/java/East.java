304740349476ce909c26665bb89963c0fe5883be
Anastasiya <Lozhkina_Anastasya@list.ru>
2024-12-18 18:14:44 +0300

    change idea of east, wait when museum is open

--- src/main/java/ru/spbstu/telematics/java/East.java
+++ src/main/java/ru/spbstu/telematics/java/East.java
@@ -11,12 +11,22 @@ public class East implements Runnable {
     public void run() {
         int visitorId = 1; // Уникальный идентификатор для посетителей
         while (true) {
-            try {
-                Thread.sleep(400); // Интервал между входами
-                if (!control.isMuseumOpen()) {
+            if (!control.isMuseumOpen()) {
+                try {
+                    System.out.println("East: Музей закрыт, поток ждет открытия.");
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
                     break;
                 }
-                control.enterMuseum(visitorId++); // Посетитель входит в музей
+            }
+            if (control.isMuseumOpen()) {
+                control.enterMuseum(visitorId++);
+            }
+
+            // Задержка между прибытием новых посетителей
+            try {
+                Thread.sleep(400);
             } catch (InterruptedException e) {
                 Thread.currentThread().interrupt();
                 break; // Завершаем поток при прерывании
