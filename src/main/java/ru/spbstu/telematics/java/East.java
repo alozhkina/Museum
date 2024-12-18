306215e8fc471172d5b8d29cde2f677d0964d8a7
Anastasiya <Lozhkina_Anastasya@list.ru>
2024-12-18 15:29:13 +0300

    add EastEnter

--- src/main/java/ru/spbstu/telematics/java/East.java
+++ src/main/java/ru/spbstu/telematics/java/East.java
@@ -1,4 +1,27 @@
 package ru.spbstu.telematics.java;
 
-public class East {
+public class East implements Runnable {
+    private final Control control;
+
+    public East(Control control) {
+        this.control = control;
+    }
+
+    @Override
+    public void run() {
+        int visitorId = 1; // Уникальный идентификатор для посетителей
+        while (true) {
+            try {
+                Thread.sleep(400); // Интервал между входами
+                if (!control.isMuseumOpen()) {
+                    break;
+                }
+                control.enterMuseum(visitorId++); // Посетитель входит в музей
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                break; // Завершаем поток при прерывании
+            }
+        }
+        System.out.println("East: Поток завершен.");
+    }
 }
